da248af1adcdeb9d6b6851bfdd10d8a609205ac4
Ox <ox_agapova@mail.ru>
2023-01-13 15:22:35 +0300

    Practical Task 1 JS done

---
+++ Conditional_statements_and_data_types.js
@@ -0,0 +1,38 @@
+//Perform addition of various types (string + boolean, string + number, number + boolean)
+
+let string = "string";
+let boolean = false;
+let number = 5;
+console.log(string + boolean);
+
+
+console.log(string + number);
+
+boolean = true;
+console.log(number + boolean);
+
+
+//Perform multiplication of various types(string * boolean, string * number, number * boolean)
+boolean = false;
+console.log(string * boolean);
+console.log(string * number);
+console.log(number * boolean);
+
+
+//Divide different types(string / boolean, string / number, number / Boolean)
+
+boolean = true;
+console.log(string / boolean);
+
+number = 15;
+console.log(string / number);
+
+number = 5;
+boolean = false;
+console.log(number / boolean);
+
+//Perform explicit conversion(number, string, boolean)
+
+/*let boolean = true;
+let number = 5+ true;
+console.log(number);*/
